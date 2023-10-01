c04b0813912de846dc2ffecc90cc77e99d6a0292
noah-d-whitney <me@noahwhitney.com>
2023-10-01 02:11:27 -0400

    More key func

--- main.c
+++ main.c
@@ -12,10 +12,13 @@ typedef struct Params {
 } params;
 
 char * generateFileName (char output[100], char * path, char * name, char * ext);
-void generateFiles(const char * filepath, char * filename);
-int file_exists (const char *filename);
-char * generateMarkup (char output[256], params *processParams, char *filename);
-int askQuestion(const char *question, char *answer, const size_t max_answer_length);
+void generateFiles		(const char * filepath, char * filename);
+int file_exists 		(const char *filename);
+char * generateMarkup 	(char output[256], params *processParams, char *filename);
+int askQuestion			(const char *question, char *answer, const size_t max_answer_length);
+void insertString		(char* destination, int pos, char* seed);
+int find_ins_index		(char* string, char* target, int ins_pos);
+
 
 int main ( int argc, char *argv[] ) {
 	char * path = getenv("PWD");
@@ -55,31 +58,47 @@ char * generateFileName (char output[100], char * path, char * name, char * ext)
 }
 
 char * generateMarkup (char output[256], params *processParams, char *filename) {
+	typedef struct Snippet {
+		char md[100];
+		int ins_pos;
+		char* target;
+	} snippet;
+
 	typedef struct Snippets {
-		char base[100];
-		char interface[50];
-		char props[20];
+		snippet base;
+		snippet interface;
+		snippet props;
 	} snippets;
 
 	// Declare snippets struct and assign markdown
-	snippets mdSnip;
-	sprintf(mdSnip.base, "const %s = () => {\n\treturn <></>;\n}\n\nexport default %s;", filename, filename);
-	sprintf(mdSnip.interface, "interface %sProps {\n\n}", filename);
-	sprintf(mdSnip.props, "props: %sProps", filename);
+	snippets md_snips;
+	int mdbuf_length;
 
-	sprintf(output, "%s\n\nconst %s = (%s) => {\n\treturn <></>;\n}\n\nexport default %s;", mdSnip.interface, filename, mdSnip.props, filename);
+	//TODO MAKE INTO MACRO
+	sprintf(md_snips.base.md, "const %s = () => {\n\treturn <></>;\n}\n\nexport default %s;", filename, filename);
+	md_snips.base.ins_pos = 0;
 
-	// sprintf(interfaceSlice, "interface %sProps {}\n\n", filename);
-	// sprintf(componentSlice, "const %s = () => {\n\treturn <></>;\n}\n\nexport default %s;", filename, filename);
-	// sprintf(componentSliceProps, "const %s = (props: %sProps) => {\n\treturn <></>;\n}\n\nexport default %s;", filename, filename, filename);
+	sprintf(md_snips.interface.md, "interface %sProps {\n\n}", filename);
+	md_snips.interface.ins_pos = 0;// CHANGE
+
+	sprintf(md_snips.props.md, "props: %sProps", filename);
+	md_snips.props.target = "st = ()";
+	md_snips.props.ins_pos = 3;
+
+	mdbuf_length = sizeof md_snips;
+	char* mdbuf = (char*)malloc(sizeof (mdbuf_length + 1));
+	// Copy base markdown to buffer
+	strcpy(mdbuf, md_snips.base.md);
+
+	// Print MDBUF
+	printf("\nMDBUF: %c\n", mdbuf[0]);
+
+	find_ins_index(mdbuf, md_snips.props.target, md_snips.props.ins_pos);
 
 
-	// if (processParams->addProps.state) {
-	// 	sprintf(output, "%s%s", interfaceSlice, componentSliceProps);
-	// } else {
-	// 	sprintf(output, "%s", componentSlice);
-	// }
+	sprintf(output, "%s\n\nconst %s = (%s) => {\n\treturn <></>;\n}\n\nexport default %s;", md_snips.interface.md, filename, md_snips.props.md, filename);
 
+	free(mdbuf);
 	return output;
 }
 
@@ -124,4 +143,48 @@ int askQuestion(const char *question, char *answer, const size_t max_answer_leng
 
     // return whether there was an error on input
     return input_ok;
+}
+
+void insertString(char* destination, int pos, char* seed)
+{
+    char * strC;
+
+    strC = (char*)malloc(strlen(destination)+strlen(seed)+1);
+    strncpy(strC,destination,pos);
+    strC[pos] = '\0';
+    strcat(strC,seed);
+    strcat(strC,destination+pos);
+    strcpy(destination,strC);
+    free(strC);
+}
+
+int find_ins_index(char* string, char* target, int ins_pos) {
+	int ins_index;
+	int matched_index = 0;
+	char matched[strlen(target)];
+
+
+	for (int i = 0; i < strlen(string); i++) {
+
+		if (string[i] == target[matched_index]) {
+			matched[matched_index] = target[matched_index]; 
+			matched_index++;
+		} else {
+			matched[0] = '\0';
+			matched_index = 0;
+		}
+
+		if (strcmp(matched, target) == 0) {
+			printf("\nTarget Found\n");
+			break;
+		}
+
+		// printf("Matched ind: %i, i: %i, cur: %c, matched: %s\n", matched_index, i, string[i], matched);
+	}
+
+	// printf("Input String: %s\n", string);
+	// printf("Target: %s\n", target);
+	// printf("Insert Position: %i\n", matched_index);
+
+	return 0;
 }
